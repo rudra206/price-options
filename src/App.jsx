7d7aac4fc846020f4e44a4154d812c7d112a59b2
rura206 <rudradipta34@gmail.com>
2025-09-13 11:02:05 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
+      <h1 className='text-7xl bg-slate-400'>Vite + React</h1>
+
+
+
+    </>
+  )
+}
+
+export default App
